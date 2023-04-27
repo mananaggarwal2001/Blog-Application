c40d03168b754e9ef72e2f47f5a18ec0d6c222b5
AnonymousManan <mananaggarwal2001@gmail.com>
2023-04-27 19:50:41 +0530

    inital commit in next js

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>This is the blog page in the react js page for putting the list of the blogs in the given application</div>
+  )
+}
+
+export default blog
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+            This is the contact js in the next js
+        </div>
+    )
+}
+
+export default contact
